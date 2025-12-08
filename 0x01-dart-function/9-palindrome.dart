626c036f56ce4999a779f95a8a7dce3ec2598464
Swagtiago29 <sant29ramos@gmail.com>
2025-12-08 17:08:39 -0300

    9 fix

--- 0x01-dart-function/9-palindrome.dart
+++ 0x01-dart-function/9-palindrome.dart
@@ -1,7 +1,9 @@
 bool isPalindrome(String s){
   int left = 0;
   int right = s.length - 1;
-
+  if (s.length < 3){
+    return false;
+  }
   while(left < right){
     if(s[left] != s[right]){
       return false;
