4c266e1e64cc1cc241fa83e72542cb6110219efd
Swagtiago29 <sant29ramos@gmail.com>
2025-12-10 14:22:27 -0300

    add 4

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,30 @@
+class Password {
+  //properties
+  String _password = "";
+
+  //constructor
+  Password({required String password}) : _password = password;
+
+  //getter
+  String get password => _password;
+
+  //setter
+  set password(String newPassword){
+    _password = newPassword;
+  }
+
+  //methods
+  bool isValid() {
+    final hasUpper = RegExp(r'[A-Z]').hasMatch(_password);
+    final hasLower = RegExp(r'[a-z]').hasMatch(_password);
+    final hasNum = RegExp(r'\d').hasMatch(_password);
+    final hasCorrectLength = _password.length >= 8 && _password.length <= 16;
+
+    return hasUpper && hasLower && hasNum && hasCorrectLength;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
